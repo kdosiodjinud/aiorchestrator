2182a09d48df6c7b157a8ee65b6405df3c5b7e8f
KdoSiOdJinud <kdosiodjinud@gmail.com>
2025-05-28 23:02:34 +0200

    fix(annotation): add anotation to tool interface

--- src/Tools/ToolInterface.php
+++ src/Tools/ToolInterface.php
@@ -4,5 +4,9 @@ namespace AiOrchestrator\Tools;
 
 interface ToolInterface {
     public function getDefinition(): array;
+
+    /**
+     * @return array{role: string, tool_call_id: string, name: string, content: string}
+     */
     public function handle($call, $args): array;
 }
